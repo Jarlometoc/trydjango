c977b845611bbb24a5412ace681b4c69518cadff
jarlometoc <shburleigh@gmail.com>
2015-09-05 22:03:29 +0200

    up to email contact form, not model form
    now up to registration

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,6 +1,16 @@
+from django import forms
+from .models import SignUp
+
+
 #for contact forms
 from django import forms
 class ContactForm(forms.Form):
     full_name = forms.CharField()
     email = forms.EmailField()
     message = forms.CharField()
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = SignUp
+        fields = ['full_name', 'email']
