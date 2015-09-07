56027274eb4c1c672bc0dbadbf4ef0bf63207b7c
jarlometoc <shburleigh@gmail.com>
2015-09-07 14:51:47 +0200

    up to email contact form, not model form now up to registration up to logon/reg links up to title change First Try

--- trydjango18/views.py
+++ trydjango18/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, 'about.html', {})   #from url newsletter.views.home
+
+def main(request):
+    if request.user.is_authenticated():
+        return render(request, 'main.html', {})   #mainpage
+    else:
+        return render(request, 'home.html', {})
